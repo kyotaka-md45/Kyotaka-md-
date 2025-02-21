f7143e2d2a81ee38abeef10cab145266192a1a89
KyotakaMD45 <daemonabraxas08@gmail.com>
2025-02-21 20:33:26 +0000

    Update ping.js

--- commands/information/ping2.js
+++ commands/information/ping2.js
@@ -9,13 +9,18 @@ module.exports = {
         if (await handler(ctx, module.exports.handler)) return;
 
         try {
+            // Envoi du message "ping"
             const startTime = performance.now();
-            const testMsg = await ctx.reply(quote("🏓 Vérification de la vitesse..."));
+            const pingMsg = await ctx.reply("🏓 Ping...");
+
+            // Calcul du temps de réponse
             const responseTime = (performance.now() - startTime).toFixed(2);
-            await ctx.editMessage(testMsg.key, quote(`🏓 Pong ! Réponse en ${responseTime} ms.`));
+
+            // Modification du message pour afficher "pong" et le temps de réponse
+            await ctx.editMessage(pingMsg.key, quote(`🏓 Pong! Temps de réponse : ${responseTime} ms.`));
         } catch (error) {
             consolefy.error(`Erreur : ${error}`);
             return await ctx.reply(quote(`⚠️ Une erreur est survenue : ${error.message}`));
         }
     }
-}
+};
