0959c21c078783ef91ccd1218ead5759d03cb8df
kyotaka-md45 <botlevanter177@gmail.com>
2025-02-23 12:30:47 +0000

    Create dalle.js

---
+++ commands/ai-image/dalle.js
@@ -0,0 +1,38 @@
+const {
+    quote
+} = require("@mengkodingan/ckptw");
+const mime = require("mime-types");
+
+module.exports = {
+    name: "dalle",
+    category: "ai-image",
+    permissions: {},
+    code: async (ctx) => {
+        const input = ctx.args.join(" ") || null;
+
+        if (!input) return await ctx.reply(
+            `${quote(tools.msg.generateInstruction(["send"], ["text"]))}\n` +
+            quote(tools.msg.generateCommandExample(ctx.used, "moon"))
+        );
+
+        try {
+            const apiUrl = tools.api.createUrl("nasirxml", "/ai/dalle3", {
+                prompt: input
+            });
+
+            return await ctx.reply({
+                image: {
+                    url: apiUrl
+                },
+                mimetype: mime.lookup("png"),
+                caption: `${quote(`Prompt: ${input}`)}\n` +
+                    "\n" +
+                    config.msg.footer
+            });
+        } catch (error) {
+            consolefy.error(`Erreur : ${error}`);
+            if (error.status !== 200) return await ctx.reply(config.msg.notFound);
+            return await ctx.reply(quote(`⚠️ Une erreur s'est produite : ${error.message}`));
+        }
+    }
+};
